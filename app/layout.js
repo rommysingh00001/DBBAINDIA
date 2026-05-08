df043c73c9ba093fa4ae7974cc4921b62bffcc62
rommysingh00001 <rommysingh00001@gmail.com>
2026-05-08 11:43:42 +0530

    Delete app/layout.js

--- app/layout.js
+++
@@ -1,9 +0,0 @@
-import './globals.css' // <--- यह लाइन सबसे ऊपर होनी चाहिए
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
