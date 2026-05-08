5b33e4670d1d9ad38effba4b71925de2156a1af9
rommysingh00001 <rommysingh00001@gmail.com>
2026-05-09 02:46:34 +0530

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -1,21 +1,112 @@
 "use client";
 
+import { useState } from "react";
+import { supabase } from "../../lib/supabase";
+import { useRouter } from "next/navigation";
+
 export default function LoginPage(){
+
+const router = useRouter();
+
+const [email,setEmail] = useState("");
+const [password,setPassword] = useState("");
+
+async function login(){
+
+const { error } =
+await supabase.auth.signInWithPassword({
+
+email,
+password
+
+});
+
+if(error){
+
+alert(error.message);
+return;
+
+}
+
+router.push("/dashboard");
+
+}
 
 return(
 
-<div style={{
-minHeight:"100vh",
-display:"flex",
-alignItems:"center",
-justifyContent:"center",
-background:"#050816",
-color:"white",
-fontSize:"40px",
-fontWeight:"bold"
-}}>
+<div className="authPage">
 
-LOGIN PAGE
+<div className="authBox">
+
+<h1>Login</h1>
+
+<input
+placeholder="Email"
+value={email}
+onChange={(e)=>
+setEmail(e.target.value)
+}
+/>
+
+<input
+type="password"
+placeholder="Password"
+value={password}
+onChange={(e)=>
+setPassword(e.target.value)
+}
+/>
+
+<button onClick={login}>
+Login
+</button>
+
+</div>
+
+<style jsx>{`
+
+.authPage{
+min-height:100vh;
+display:flex;
+align-items:center;
+justify-content:center;
+background:#020617;
+}
+
+.authBox{
+width:400px;
+background:#111827;
+padding:40px;
+border-radius:20px;
+display:flex;
+flex-direction:column;
+gap:20px;
+}
+
+.authBox h1{
+color:white;
+text-align:center;
+}
+
+input{
+padding:14px;
+border:none;
+border-radius:12px;
+background:#1e293b;
+color:white;
+}
+
+button{
+padding:14px;
+border:none;
+border-radius:12px;
+background:#06b6d4;
+color:white;
+font-weight:700;
+cursor:pointer;
+}
+
+`}</style>
 
 </div>
 
