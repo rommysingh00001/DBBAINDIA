10e0aafa25ee7047ac2757eaea7984e834bc365c
rommysingh00001 <rommysingh00001@gmail.com>
2026-05-09 00:28:40 +0530

    Delete app/layout.js

--- app/layout.js
+++
@@ -1,7 +0,0 @@
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body style={{ margin: 0, backgroundColor: '#0d1117' }}>{children}</body>
-    </html>
-  )
-}
