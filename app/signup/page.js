a196063cff27e24451aa7bf7862c5b6d3c5195b5
rommysingh00001 <rommysingh00001@gmail.com>
2026-05-09 03:06:06 +0530

    Update page.js

--- app/signup/page.js
+++ app/signup/page.js
@@ -2,19 +2,15 @@
 
 import { useState } from "react";
 import { supabase } from "../../lib/supabase";
-import { useRouter } from "next/navigation";
 
 export default function SignupPage(){
 
-const router = useRouter();
-
-const [name,setName] = useState("");
 const [email,setEmail] = useState("");
 const [password,setPassword] = useState("");
 
 async function signup(){
 
-const { error } =
+const { data,error } =
 await supabase.auth.signUp({
 
 email,
@@ -22,39 +18,46 @@ password
 
 });
 
-if(error){
-
+console.log(data);
 console.log(error);
 
-alert(
-error?.message ||
-"Signup Failed"
-);
+if(error){
+
+alert(error.message);
 return;
 
 }
 
 alert("Signup Successful");
 
-router.push("/login");
-
 }
 
 return(
 
-<div className="authPage">
+<div style={{
+minHeight:"100vh",
+display:"flex",
+justifyContent:"center",
+alignItems:"center",
+background:"#020617"
+}}>
 
-<div className="authBox">
+<div style={{
+width:"350px",
+background:"#111827",
+padding:"30px",
+borderRadius:"20px",
+display:"flex",
+flexDirection:"column",
+gap:"15px"
+}}>
 
-<h1>Signup</h1>
-
-<input
-placeholder="Name"
-value={name}
-onChange={(e)=>
-setName(e.target.value)
-}
-/>
+<h1 style={{
+color:"white",
+textAlign:"center"
+}}>
+Signup
+</h1>
 
 <input
 placeholder="Email"
@@ -62,6 +65,10 @@ value={email}
 onChange={(e)=>
 setEmail(e.target.value)
 }
+style={{
+padding:"14px",
+borderRadius:"10px"
+}}
 />
 
 <input
@@ -71,58 +78,27 @@ value={password}
 onChange={(e)=>
 setPassword(e.target.value)
 }
+style={{
+padding:"14px",
+borderRadius:"10px"
+}}
 />
 
-<button onClick={signup}>
+<button
+onClick={signup}
+style={{
+padding:"14px",
+borderRadius:"10px",
+background:"#06b6d4",
+color:"white",
+fontWeight:"bold",
+border:"none"
+}}
+>
 Signup
 </button>
 
 </div>
-
-<style jsx>{`
-
-.authPage{
-min-height:100vh;
-display:flex;
-align-items:center;
-justify-content:center;
-background:#020617;
-}
-
-.authBox{
-width:400px;
-background:#111827;
-padding:40px;
-border-radius:20px;
-display:flex;
-flex-direction:column;
-gap:20px;
-}
-
-.authBox h1{
-color:white;
-text-align:center;
-}
-
-input{
-padding:14px;
-border:none;
-border-radius:12px;
-background:#1e293b;
-color:white;
-}
-
-button{
-padding:14px;
-border:none;
-border-radius:12px;
-background:#06b6d4;
-color:white;
-font-weight:700;
-cursor:pointer;
-}
-
-`}</style>
 
 </div>
 
