b7d91f119eb306cdd4a10975304ad1aa8b8a036d
rommysingh00001 <rommysingh00001@gmail.com>
2026-05-09 03:38:06 +0530

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -4,22 +4,21 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { supabase } from "../../lib/supabase";
 
-export default function LoginPage(){
+export default function LoginPage() {
 
 const router = useRouter();
 
-const [phone,setPhone] = useState("");
+const [phone, setPhone] = useState("");
 
-async function login(){
+async function login() {
 
-const { data,error } =
-await supabase
+const { data, error } = await supabase
 .from("users")
 .select("*")
-.eq("phone",phone)
+.eq("phone", phone)
 .single();
 
-if(error || !data){
+if (error || !data) {
 
 alert("User not found");
 return;
@@ -35,29 +34,29 @@ router.push("/dashboard");
 
 }
 
-return(
+return (
 
 <div style={{
-minHeight:"100vh",
-display:"flex",
-justifyContent:"center",
-alignItems:"center",
-background:"#020617"
+minHeight: "100vh",
+display: "flex",
+justifyContent: "center",
+alignItems: "center",
+background: "#020617"
 }}>
 
 <div style={{
-width:"350px",
-background:"#111827",
-padding:"30px",
-borderRadius:"20px",
-display:"flex",
-flexDirection:"column",
-gap:"15px"
+width: "350px",
+background: "#111827",
+padding: "30px",
+borderRadius: "20px",
+display: "flex",
+flexDirection: "column",
+gap: "15px"
 }}>
 
 <h1 style={{
-color:"white",
-textAlign:"center"
+color: "white",
+textAlign: "center"
 }}>
 Login
 </h1>
@@ -65,24 +64,24 @@ Login
 <input
 placeholder="Phone Number"
 value={phone}
-onChange={(e)=>
-setPhone(e.target.value)
-}
+onChange={(e) => setPhone(e.target.value)}
 style={{
-padding:"14px",
-borderRadius:"10px"
+padding: "14px",
+borderRadius: "10px",
+border: "none"
 }}
 />
 
 <button
 onClick={login}
 style={{
-padding:"14px",
-borderRadius:"10px",
-background:"#06b6d4",
-color:"white",
-fontWeight:"bold",
-border:"none"
+padding: "14px",
+borderRadius: "10px",
+background: "#06b6d4",
+color: "white",
+fontWeight: "bold",
+border: "none",
+cursor: "pointer"
 }}
 >
 Login
@@ -92,6 +91,6 @@ Login
 
 </div>
 
-)
+);
 
 }
