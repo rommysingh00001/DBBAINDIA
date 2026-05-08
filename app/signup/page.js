43e69b273001fb04284d1ce1bd56bdc8bf1f3c39
rommysingh00001 <rommysingh00001@gmail.com>
2026-05-09 03:27:28 +0530

    Update page.js

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,3 +1,16 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { supabase } from "../../lib/supabase";
+
+export default function SignupPage(){
+
+const router = useRouter();
+
+const [name,setName] = useState("");
+const [phone,setPhone] = useState("");
+
 async function signup(){
 
 const { error } =
@@ -6,7 +19,7 @@ await supabase
 .insert([{
 
 name,
-email,
+phone,
 wallet:0
 
 }]);
@@ -21,5 +34,78 @@ return;
 alert("Signup Successful");
 
 router.push("/login");
+
+}
+
+return(
+
+<div style={{
+minHeight:"100vh",
+display:"flex",
+justifyContent:"center",
+alignItems:"center",
+background:"#020617"
+}}>
+
+<div style={{
+width:"350px",
+background:"#111827",
+padding:"30px",
+borderRadius:"20px",
+display:"flex",
+flexDirection:"column",
+gap:"15px"
+}}>
+
+<h1 style={{
+color:"white",
+textAlign:"center"
+}}>
+Signup
+</h1>
+
+<input
+placeholder="Name"
+value={name}
+onChange={(e)=>
+setName(e.target.value)
+}
+style={{
+padding:"14px",
+borderRadius:"10px"
+}}
+/>
+
+<input
+placeholder="Phone Number"
+value={phone}
+onChange={(e)=>
+setPhone(e.target.value)
+}
+style={{
+padding:"14px",
+borderRadius:"10px"
+}}
+/>
+
+<button
+onClick={signup}
+style={{
+padding:"14px",
+borderRadius:"10px",
+background:"#06b6d4",
+color:"white",
+fontWeight:"bold",
+border:"none"
+}}
+>
+Signup
+</button>
+
+</div>
+
+</div>
+
+)
 
 }
