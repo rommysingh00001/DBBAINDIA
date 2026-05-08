e1766d27fce08bafdd332570109aeb1cca9223b6
rommysingh00001 <rommysingh00001@gmail.com>
2026-05-08 21:26:43 +0530

    Create page.js

---
+++ app/profile/page.js
@@ -0,0 +1,80 @@
+'use client'
+
+import { useEffect,useState }
+from 'react'
+
+import { useRouter }
+from 'next/navigation'
+
+import '../../app/globals.css'
+
+import Navbar
+from '../../components/Navbar'
+
+export default function Profile(){
+
+  const router = useRouter()
+
+  const [user,setUser] =
+    useState(null)
+
+  useEffect(()=>{
+
+    const localUser =
+      localStorage.getItem(
+        'dbbaUser'
+      )
+
+    if(localUser){
+
+      setUser(
+        JSON.parse(localUser)
+      )
+    }
+
+  },[])
+
+  function logout(){
+
+    localStorage.removeItem(
+      'dbbaUser'
+    )
+
+    router.push('/')
+  }
+
+  return(
+
+    <main className="pageContainer">
+
+      <h1 className="pageTitle">
+        My Profile
+      </h1>
+
+      <div className="profileCard">
+
+        <h2>
+          {user?.name}
+        </h2>
+
+        <p>
+          {user?.email}
+        </p>
+
+        <p>
+          {user?.phone}
+        </p>
+
+        <button
+          onClick={logout}
+        >
+          Logout
+        </button>
+
+      </div>
+
+      <Navbar/>
+
+    </main>
+  )
+}
