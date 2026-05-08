d8165527c349096c98dfa58ad93e29645dd7892f
rommysingh00001 <rommysingh00001@gmail.com>
2026-05-08 15:41:48 +0530

    Create NumberGrid.jsx

---
+++ components/NumberGrid.jsx
@@ -0,0 +1,33 @@
+export default function NumberGrid({
+  numbers,
+  selected,
+  onSelect
+}) {
+
+  return (
+
+    <div className="numberGrid">
+
+      {numbers.map((num)=>(
+
+        <button
+          key={num}
+
+          className={
+            selected === num
+            ? 'numberBtn active'
+            : 'numberBtn'
+          }
+
+          onClick={()=>
+            onSelect(num)
+          }
+        >
+          {num}
+        </button>
+
+      ))}
+
+    </div>
+  );
+}
