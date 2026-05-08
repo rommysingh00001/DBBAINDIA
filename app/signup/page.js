b6f9ffe89ee237171d4733329976c0c76a1e49f9
rommysingh00001 <rommysingh00001@gmail.com>
2026-05-09 03:22:23 +0530

    Update page.js

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,25 +1,15 @@
-"use client";
-
-import { useState } from "react";
-import { supabase } from "../../lib/supabase";
-
-export default function SignupPage(){
-
-const [email,setEmail] = useState("");
-const [password,setPassword] = useState("");
-
 async function signup(){
 
-const { data,error } =
-await supabase.auth.signUp({
+const { error } =
+await supabase
+.from("users")
+.insert([{
 
+name,
 email,
-password
+wallet:0
 
-});
-
-console.log(data);
-console.log(error);
+}]);
 
 if(error){
 
@@ -30,78 +20,6 @@ return;
 
 alert("Signup Successful");
 
-}
-
-return(
-
-<div style={{
-minHeight:"100vh",
-display:"flex",
-justifyContent:"center",
-alignItems:"center",
-background:"#020617"
-}}>
-
-<div style={{
-width:"350px",
-background:"#111827",
-padding:"30px",
-borderRadius:"20px",
-display:"flex",
-flexDirection:"column",
-gap:"15px"
-}}>
-
-<h1 style={{
-color:"white",
-textAlign:"center"
-}}>
-Signup
-</h1>
-
-<input
-placeholder="Email"
-value={email}
-onChange={(e)=>
-setEmail(e.target.value)
-}
-style={{
-padding:"14px",
-borderRadius:"10px"
-}}
-/>
-
-<input
-type="password"
-placeholder="Password"
-value={password}
-onChange={(e)=>
-setPassword(e.target.value)
-}
-style={{
-padding:"14px",
-borderRadius:"10px"
-}}
-/>
-
-<button
-onClick={signup}
-style={{
-padding:"14px",
-borderRadius:"10px",
-background:"#06b6d4",
-color:"white",
-fontWeight:"bold",
-border:"none"
-}}
->
-Signup
-</button>
-
-</div>
-
-</div>
-
-)
+router.push("/login");
 
 }
