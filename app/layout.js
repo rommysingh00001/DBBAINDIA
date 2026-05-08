c869ae28101309b49fc4fc96883453c1d94a6681
rommysingh00001 <rommysingh00001@gmail.com>
2026-05-08 11:09:03 +0530

    layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,9 @@
+import './globals.css' // <--- यह लाइन सबसे ऊपर होनी चाहिए
+
 export default function RootLayout({ children }) {
-  return (<html><body>{children}</body></html>)
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
