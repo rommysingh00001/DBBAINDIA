ad97811397c5e23ac64524ea822d40d215dc75f4
rommysingh00001 <rommysingh00001@gmail.com>
2026-05-08 16:12:39 +0530

    Update page.js

--- app/howtoplay/page.js
+++ app/howtoplay/page.js
@@ -1,13 +1,33 @@
-import '../../app/globals.css';
+import '../globals.css'
 
 export default function HowToPlay() {
 
   return (
 
-    <main className="dashboard">
+    <main className="page">
 
       <h1>How To Play</h1>
 
+      <div className="steps">
+
+        <div className="step">
+          Select Number
+        </div>
+
+        <div className="step">
+          Enter Amount
+        </div>
+
+        <div className="step">
+          Place Bet
+        </div>
+
+        <div className="step">
+          Check Results
+        </div>
+
+      </div>
+
     </main>
-  );
+  )
 }
