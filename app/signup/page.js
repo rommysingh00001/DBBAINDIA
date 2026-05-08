30e73c562b4694c2169ca85802c2ec26a7671909
rommysingh00001 <rommysingh00001@gmail.com>
2026-05-08 20:59:18 +0530

    Update page.js

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,34 +1,87 @@
 'use client'
 
-import '../globals.css'
+import { useState } from 'react'
 import { useRouter } from 'next/navigation'
+import { supabase } from '../../lib/supabase'
+import '../globals.css'
 
 export default function Signup() {
 
   const router = useRouter()
 
+  const [name, setName] =
+    useState('')
+
+  const [email, setEmail] =
+    useState('')
+
+  const [phone, setPhone] =
+    useState('')
+
+  const [password, setPassword] =
+    useState('')
+
+  async function signup() {
+
+    await supabase
+    .from('users')
+    .insert([
+      {
+        name,
+        email,
+        phone,
+        password,
+        wallet:1000
+      }
+    ])
+
+    alert('Signup Successful')
+
+    router.push('/')
+  }
+
   return (
 
-    <main className="loginPage">
+    <main className="authPage">
 
-      <div className="loginBox">
+      <div className="authBox">
 
         <h1>Create Account</h1>
 
-        <input placeholder="Name" />
-
-        <input placeholder="Mobile" />
-
         <input
-          placeholder="Password"
-          type="password"
+          placeholder="Name"
+          value={name}
+          onChange={(e)=>
+            setName(e.target.value)
+          }
         />
 
-        <button
-          onClick={() =>
-            router.push('/')
+        <input
+          placeholder="Email"
+          value={email}
+          onChange={(e)=>
+            setEmail(e.target.value)
           }
-        >
+        />
+
+        <input
+          placeholder="Phone Number"
+          value={phone}
+          onChange={(e)=>
+            setPhone(e.target.value)
+          }
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e)=>
+            setPassword(e.target.value)
+          }
+        />
+
+        <button onClick={signup}>
           Signup
         </button>
 
