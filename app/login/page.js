d310b4843cd1d9a7fc443b7479a8e32e9ec26559
rommysingh00001 <rommysingh00001@gmail.com>
2026-05-09 03:25:01 +0530

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -1,15 +1,14 @@
 "use client";
 
 import { useState } from "react";
-import { supabase } from "../../lib/supabase";
 import { useRouter } from "next/navigation";
+import { supabase } from "../../lib/supabase";
 
 export default function LoginPage(){
 
 const router = useRouter();
 
 const [email,setEmail] = useState("");
-const [password,setPassword] = useState("");
 
 async function login(){
 
@@ -35,24 +34,33 @@ JSON.stringify(data)
 router.push("/dashboard");
 
 }
-if(error){
-
-alert(error.message);
-return;
-
-}
-
-router.push("/dashboard");
-
-}
 
 return(
 
-<div className="authPage">
+<div style={{
+minHeight:"100vh",
+display:"flex",
+justifyContent:"center",
+alignItems:"center",
+background:"#020617"
+}}>
 
-<div className="authBox">
+<div style={{
+width:"350px",
+background:"#111827",
+padding:"30px",
+borderRadius:"20px",
+display:"flex",
+flexDirection:"column",
+gap:"15px"
+}}>
 
-<h1>Login</h1>
+<h1 style={{
+color:"white",
+textAlign:"center"
+}}>
+Login
+</h1>
 
 <input
 placeholder="Email"
@@ -60,67 +68,27 @@ value={email}
 onChange={(e)=>
 setEmail(e.target.value)
 }
+style={{
+padding:"14px",
+borderRadius:"10px"
+}}
 />
 
-<input
-type="password"
-placeholder="Password"
-value={password}
-onChange={(e)=>
-setPassword(e.target.value)
-}
-/>
-
-<button onClick={login}>
+<button
+onClick={login}
+style={{
+padding:"14px",
+borderRadius:"10px",
+background:"#06b6d4",
+color:"white",
+fontWeight:"bold",
+border:"none"
+}}
+>
 Login
 </button>
 
 </div>
-
-<style jsx>{`
-
-.authPage{
-min-height:100vh;
-display:flex;
-align-items:center;
-justify-content:center;
-background:#020617;
-}
-
-.authBox{
-width:400px;
-background:#111827;
-padding:40px;
-border-radius:20px;
-display:flex;
-flex-direction:column;
-gap:20px;
-}
-
-.authBox h1{
-color:white;
-text-align:center;
-}
-
-input{
-padding:14px;
-border:none;
-border-radius:12px;
-background:#1e293b;
-color:white;
-}
-
-button{
-padding:14px;
-border:none;
-border-radius:12px;
-background:#06b6d4;
-color:white;
-font-weight:700;
-cursor:pointer;
-}
-
-`}</style>
 
 </div>
 
