20c003d4b4167c7851deb8e9b1a32ee3513bb6c3
rommysingh00001 <rommysingh00001@gmail.com>
2026-05-08 23:59:19 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body style={{ margin: 0, backgroundColor: '#0d1117' }}>{children}</body>
     </html>
   )
 }
