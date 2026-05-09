171fa466f87c16733c6b894ba925e989584b7755
rommysingh00001 <rommysingh00001@gmail.com>
2026-05-09 10:06:57 +0530

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -1,16 +1,3 @@
-"use client";
-
-import { useState } from "react";
-import { useRouter } from "next/navigation";
-import { supabase } from "../../lib/supabase";
-
-export default function LoginPage(){
-
-const router = useRouter();
-
-const [email,setEmail] = useState("");
-const [password,setPassword] = useState("");
-
 async function login(){
 
 const { data,error } = await supabase
@@ -33,78 +20,5 @@ JSON.stringify(data)
 );
 
 window.location.href = "/dashboard";
-
-}
-
-return(
-
-<div style={{
-minHeight:"100vh",
-display:"flex",
-justifyContent:"center",
-alignItems:"center",
-background:"#020617"
-}}>
-
-<div style={{
-width:"350px",
-background:"#111827",
-padding:"30px",
-borderRadius:"20px",
-display:"flex",
-flexDirection:"column",
-gap:"15px"
-}}>
-
-<h1 style={{
-color:"white",
-textAlign:"center"
-}}>
-Login
-</h1>
-
-<input
-placeholder="Email"
-value={email}
-onChange={(e)=>setEmail(e.target.value)}
-style={{
-padding:"14px",
-borderRadius:"10px",
-border:"none"
-}}
-/>
-
-<input
-type="password"
-placeholder="Password"
-value={password}
-onChange={(e)=>setPassword(e.target.value)}
-style={{
-padding:"14px",
-borderRadius:"10px",
-border:"none"
-}}
-/>
-
-<button
-onClick={login}
-style={{
-padding:"14px",
-borderRadius:"10px",
-background:"#06b6d4",
-color:"white",
-fontWeight:"bold",
-border:"none",
-cursor:"pointer"
-}}
->
-Login
-</button>
-
-</div>
-
-</div>
-
-)
 
 }
