6bf80899feaa8f708c2b7c6477b8fda2075f8a06
DBBA INDIA <rommysingh00001@gmail.com>
2026-05-20 07:00:02 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,24 +2,16 @@ import {
   createClient
 } from "https://esm.sh/@supabase/supabase-js@2";
 
-// =========================
-// SUPABASE CONFIG
-// =========================
-
 const supabaseUrl =
   "https://ooqbjzhgezkdxwhmxlxp.supabase.co";
 
-const supabaseAnonKey =
+const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vcWJqemhnZXprZHh3aG14bHhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxOTQ4MzcsImV4cCI6MjA5NDc3MDgzN30.5zAmVsS9FImD_BfJ8S6eSuX3rBVhuNYfjaPbUjOV-rE";
-
-// =========================
-// CREATE CLIENT
-// =========================
 
 export const supabase =
   createClient(
     supabaseUrl,
-    supabaseAnonKey,
+    supabaseKey,
     {
 
       auth: {
@@ -48,33 +40,3 @@ export const supabase =
 
     }
   );
-
-// =========================
-// SESSION CHECK
-// =========================
-
-const {
-  data: { session }
-} = await supabase.auth.getSession();
-
-console.log("Current Session:", session);
-
-// =========================
-// AUTH STATE LISTENER
-// =========================
-
-supabase.auth.onAuthStateChange(
-  (event, session) => {
-
-    console.log(
-      "Auth Event:",
-      event
-    );
-
-    console.log(
-      "Session:",
-      session
-    );
-
-  }
-);
